3a8d3954aa42f1ad22a15833f3608b911443903e
Yaseen Nur <paradushex@gmail.com>
2022-05-04 15:29:07 +0600

    Added firebase as a database for testing and added error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,35 +1,49 @@
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://food-order-app-a8939-default-rtdb.asia-southeast1.firebasedatabase.app/food-order-app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something Went Wrong");
+      }
+      const responseDta = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseDta) {
+        console.log(key);
+        loadedMeals.push({
+          id: key,
+          name: responseDta[key].name,
+          description: responseDta[key].description,
+          price: +responseDta[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchMeals().catch((error) => {
+      setError(error.message);
+      setIsLoading(false);
+    });
+
+    // return () => {
+    //   cleanup;
+    // };
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -41,6 +55,8 @@ const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
       <Card>
+        {isLoading && <h1 style={{ textAlign: "center" }}>LOADING...</h1>}
+        {error && <h1 style={{ textAlign: "center" }}>{error}</h1>}
         <ul>{mealsList}</ul>
       </Card>
     </section>
